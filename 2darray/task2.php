007851f77b8d44a8e4d2ec0812d48b6adb5e35cc
Dmitry-Skavinski <d.skavinskiy@mail.ru>
2023-03-23 11:25:38 +0300

    2d arrays task 2

---
+++ 2darray/task2.php
@@ -0,0 +1,33 @@
+<?php
+function leastAverage(array $matrix): int
+{
+    $index = 0;
+    $leastAverage = PHP_FLOAT_MAX;
+    for ($i = 0; $i < count($matrix); $i++) {
+        $sum = 0;
+        $count = 0;
+        for ($j = 0; $j < count($matrix[$i]); $j++) {
+            if ($matrix[$i][$j] > 0) {
+                $count++;
+                $sum+= $matrix[$i][$j];
+            }
+        }
+
+        $average = $count ? $sum / $count : 0;
+
+        if ($average < $leastAverage) {
+            $leastAverage = $average;
+            $index = $i;
+        }
+    }
+
+    return $i;
+}
+
+$matrix = [
+    [1, 2, 3],
+    [2, 3, 1],
+    [1, 2, 2],
+];
+
+echo leastAverage($matrix) . PHP_EOL; // 3
